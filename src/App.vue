94eb30ec48d03d21990675fab11c7a264fe66fc9
binyy <408460486@qq.com>
2019-09-27 11:49:18 +0800

    20190827

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app"><VHeader></VHeader></div>
+</template>
+
+<script>
+import VHeader from './components/v-header/v-header.vue';
+
+export default {
+  name: 'app',
+  components: {
+    VHeader
+  }
+};
+</script>
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 0px
+</style>
---
+++ src/components/v-header/v-header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <cube-button>TEST</cube-button>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
